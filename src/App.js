352ec1f800f8d8e18384629d194ef00ad0bb6497
Mark Mironyuk <contact@markmiro.com>
2019-03-18 22:21:52 -0700

    Basic setup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <h1>Pet Store</h1>
+      <Router>
+        <div>
+          <ul className="App__nav">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/store">Store</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+            <li>Create Account</li>
+            <li>Login</li>
+          </ul>
+
+          <hr />
+
+          <Route exact path="/" component={Home} />
+          <Route path="/store" component={Store} />
+          <Route path="/users" component={Users} />
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+function Store() {
+  return (
+    <div>
+      <h1>Store</h1>
+    </div>
+  );
+}
+
+function Users() {
+  return (
+    <div>
+      <h1>Users</h1>
+    </div>
+  );
 }
 
 export default App;
