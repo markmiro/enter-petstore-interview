b30f738dd144b92c53daa14fac3977e66337f7c6
Mark Mironyuk <contact@markmiro.com>
2019-03-19 01:35:56 -0700

    Finish most of the styling

--- src/App.js
+++ src/App.js
@@ -6,61 +6,15 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <div className="App__header">
-          <h1 className="f5 mv0 b">Pet Store</h1>
-          <div className="App__header__avatar">SM</div>
-        </div>
-        <div className="nested-list-reset">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/store">Store</Link>
-              <ul>
-                <li>
-                  <Field label="Find Pet by ID" type="search" />
-                </li>
-                <li>
-                  <Link to="/store/add-pet">Add Pet</Link>
-                </li>
-                <li>
-                  <TODO>Order</TODO>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-              <ul>
-                <li>
-                  <Link to="/users/create">Create Account</Link>
-                </li>
-                <li>
-                  <Link to="/users/login">Login</Link>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-
-        <div className="App__body">
-          <Route exact path="/" component={Home} />
-          <Route path="/store" component={Store} />
-          <Route path="/store/add-pet" component={CreatePet} />
-          <Route exact path="/users" component={Users} />
-          <Route path="/users/create" component={CreateUser} />
-          <Route path="/users/login" component={LoginUser} />
-        </div>
+        <Route exact path="/" component={LoginUser} />
+        <Route exact path="/store" component={Store} />
+        <Route path="/store/add-pet" component={CreatePet} />
+        <Route path="/store/order-pet" component={OrderPet} />
+        <Route exact path="/users" component={Users} />
+        <Route path="/users/create" component={CreateUser} />
+        <Route path="/users/login" component={LoginUser} />
       </div>
     </Router>
-  );
-}
-
-function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
   );
 }
 
@@ -70,59 +24,67 @@ function Store() {
   React.useEffect(() => {
     fetch("https://petstore.swagger.io/v2/store/inventory")
       .then(r => r.json())
-      .then(json => {
-        // As an array of [pet name, status] pairs
-        const asPairs = Object.keys(json).map(k => ({
-          name: k,
-          status: json[k]
-        }));
-        setPets(asPairs);
-      });
+      .then(setPets);
   });
 
   return (
-    <div>
+    <AuthenticatedPage>
       <h1>Store</h1>
-      <h2>Pets</h2>
-      <TODO>Verify that it's status codes to quantities, not pet names</TODO>
-      <ul>
-        {pets.map(pet => (
-          <li>{pet.name}</li>
-        ))}
-      </ul>
-    </div>
+      <Field label="Find Pet by ID" type="search" />
+      Available: {pets.available} • Pending: {pets.pending} • Sold: {pets.sold}
+    </AuthenticatedPage>
   );
 }
 
 function CreatePet() {
   return (
-    <div>
+    <AuthenticatedPage>
       <h1>Add Pet</h1>
-      <form>
+      <form className="card">
         <Field label="Name" />
         <Field label="Status" />
         <Field label="Category" />
         <Field label="Tags" />
-        <TODO>Show photos</TODO>
         <Field label="Upload Image" type="file" />
+        <button type="submit">Add Pet</button>
       </form>
-    </div>
+    </AuthenticatedPage>
+  );
+}
+
+function OrderPet() {
+  return (
+    <AuthenticatedPage>
+      <h1>Order Pet</h1>
+      <p className="card">Different than adding a pet?</p>
+    </AuthenticatedPage>
   );
 }
 
 function Users() {
   return (
-    <div>
+    <AuthenticatedPage>
       <h1>Users</h1>
-    </div>
+      <div className="card">
+        <ul>
+          <li>Add List of Users</li>
+        </ul>
+      </div>
+    </AuthenticatedPage>
   );
 }
 
 function CreateUser() {
   return (
-    <div>
+    <div className="App__login pa4">
       <h1>Create Account</h1>
-      <form>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          alert("create account worked (fake)");
+          window.location = "/store";
+        }}
+      >
         <Field label="Username" />
         <Field label="First Name" />
         <Field label="Last Name" />
@@ -137,14 +99,86 @@ function CreateUser() {
 
 function LoginUser() {
   return (
-    <div>
+    <div className="App__login pa4">
       <h1>Login</h1>
       <form>
         <Field label="Username" />
         <Field label="Password" type="password" />
-        <button type="submit">Login</button>
+        <button
+          type="submit"
+          onClick={e => {
+            e.preventDefault();
+            // TODO: do login, then...
+            window.location = "/store";
+          }}
+        >
+          Login
+        </button>
+        <small className="db mt2 tc">
+          Don't have an account? <Link to="/users/create">Sign up</Link>
+        </small>
       </form>
     </div>
+  );
+}
+
+function AuthenticatedPage({ children }) {
+  return (
+    <>
+      <Header />
+      <div className="flex flex-wrap">
+        <Sidebar />
+        <div className="App__body">{children}</div>
+      </div>
+    </>
+  );
+}
+
+function Sidebar() {
+  return (
+    <div className="nested-list-reset">
+      <ul>
+        <li>
+          <Link className="nav-link" to="/store">
+            Store
+          </Link>
+          <ul>
+            <li>
+              <Link className="nav-link" to="/store/add-pet">
+                Add Pet
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/store/order-pet">
+                Order Pet
+              </Link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <Link className="nav-link" to="/users">
+            Users
+          </Link>
+        </li>
+        <li>
+          {/* TODO: make this actually work */}
+          <Link className="nav-link" to="/users/login">
+            Logout
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <>
+      <div className="App__header">
+        <h1 className="f5 mv0 b">Pet Store</h1>
+        <div className="App__header__avatar">SM</div>
+      </div>
+    </>
   );
 }
 
@@ -159,10 +193,6 @@ function Field({ label, className, type, ...rest }) {
       <input className={`db mb3 ${className}`} type={type} {...rest} />
     </>
   );
-}
-
-function TODO({ children }) {
-  return <div className="gray b">TODO: {children}</div>;
 }
 
 export default App;
